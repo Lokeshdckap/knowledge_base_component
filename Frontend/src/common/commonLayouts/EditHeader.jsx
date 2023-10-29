2e808033b61ed696e8ac2d097338db34ba4e6511
Lokeshdckap <lokeshcdckap@gmail.com>
2023-10-29 14:12:53 +0530

    Input Error Solved

--- Frontend/src/common/commonLayouts/EditHeader.jsx
+++ Frontend/src/common/commonLayouts/EditHeader.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+// import axiosClient from "../../axios-client";
+// import { useParams } from "react-router-dom";
 
 export default function EditHeader(props) {
-  const renderscript = props.renderScript
-  // console.log(renderscript.title);
+
 
   return (
     <div className="">
@@ -10,7 +11,7 @@ export default function EditHeader(props) {
         className={`flex items-center justify-between m-auto ${props.widths} mt-3 space-y-2`}
       >
         {/* <div className="box-border bg-[#EEEEEE] h-11 w-72 pl-2 pt-1 border-[1px] rounded"> */}
-          <input className="text-primary font-bold text-2xl bg-[#EEEEEE] focus:outline-gray-300 w-80 h-11 pl-2 rounded" value={renderscript.title}  onChange={props.changeEvent}/>
+          <input className="text-primary font-bold text-2xl bg-[#EEEEEE] focus:outline-gray-300 w-80 h-11 pl-2 rounded" value={props.inputValue} onChange={(e)=>props.changeEvent(e.target.value)}  />
         {/* </div> */}
         <div >
           <button
